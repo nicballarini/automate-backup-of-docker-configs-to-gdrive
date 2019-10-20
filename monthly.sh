6a02552f465895e85309ab983f47ee7c0a21d257
nicballarini <13786995+nicballarini@users.noreply.github.com>
2019-10-19 19:25:17 -0500

    updating existing scripts and created new script files
    I added 3 script files: config, create_folder_structure & initial_setup scripts.
    
    Cleaned up monthly & weekly.

--- monthly.sh
+++ monthly.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Define vars
-PATH_TO_DOCKER_CONFIGS=[INPUT_YOUR_DOCKER_CONFIGS_PATH]
+PATH_TO_DOCKER_CONFIGS=/[INPUT_YOUR_DOCKER_CONFIGS_PATH]
 DOCKER_MONTHLY_BACKUP_FILENAME=docker2.$(date +%Y-%m).tar.bz2
 PARENT_ID_MONTHLY_FOLDER=[INPUT_FOLDER_ID_FROM_GDRIVE]
 
 #zip the given dir using and saving using generated filename
-tar -cvjf /backup/monthly/$DOCKER_MONTHLY_BACKUP_FILENAME /$PATH_TO_DOCKER_CONFIGS/
+tar -cvjf /backup/monthly/$DOCKER_MONTHLY_BACKUP_FILENAME $PATH_TO_DOCKER_CONFIGS/
 
 #sync that file to gdrive to referenced parent dir
 gdrive upload --parent $PARENT_ID_MONTHLY_FOLDER /backup/monthly/$DOCKER_MONTHLY_BACKUP_FILENAME
--- weekly.sh
+++ weekly.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 #Define vars
-PATH_TO_DOCKER_CONFIGS=[INPUT_YOUR_DOCKER_CONFIGS_PATH]
+PATH_TO_DOCKER_CONFIGS=/[INPUT_YOUR_DOCKER_CONFIGS_PATH]
 PARENT_ID_WEEKLY_FOLDER=[INPUT_FOLDER_ID_FROM_GDRIVE]
 
 #I'm backing up each docker container group separately to make restoring more granular. If I screw up a small piece, there's no reason to restore the entire lot.
 #
 #
 #Here's a template to create any additional tars
-#tar -cvjf /backup/weekly/docker2.tknails-site.tar.bz2 /$PATH_TO_DOCKER_CONFIGS/[ANOTHER-DIR]
+#tar -cvjf /backup/weekly/[HOSTNAME].[DOCKER_CONFIG_GROUP_FOLDER].tar.bz2 /$PATH_TO_DOCKER_CONFIGS/[DOCKER_CONFIG_GROUP_FOLDER]
 
 
 #scripts
-tar -cvjf /backup/weekly/docker2.scripts.tar.bz2 /$PATH_TO_DOCKER_CONFIGS/scripts
+tar -cvjf /backup/weekly/docker2.scripts.tar.bz2 $PATH_TO_DOCKER_CONFIGS/scripts
 
 #homeautomation
-tar -cvjf /backup/weekly/docker2.homeautomation.tar.bz2 /$PATH_TO_DOCKER_CONFIGS/homeautomation
+tar -cvjf /backup/weekly/docker2.homeautomation.tar.bz2 $PATH_TO_DOCKER_CONFIGS/homeautomation
 
 #utils
-tar -cvjf /backup/weekly/docker2.utils.tar.bz2 /$PATH_TO_DOCKER_CONFIGS/utils
+tar -cvjf /backup/weekly/docker2.utils.tar.bz2 $PATH_TO_DOCKER_CONFIGS/utils
 
 #plexland
-tar -cvjf /backup/weekly/docker2.plexland.tar.bz2 /$PATH_TO_DOCKER_CONFIGS/plexland
+tar -cvjf /backup/weekly/docker2.plexland.tar.bz2 $PATH_TO_DOCKER_CONFIGS/plexland
 
 #used to sync zips to gdrive
 gdrive sync upload /backup/weekly/ $PARENT_ID_WEEKLY_FOLDER
